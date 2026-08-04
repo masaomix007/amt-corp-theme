8371332b4cceb703f310c0ae9108fbcf6bf8bcb6
masaomix007 <bteahad@gmail.com>
2026-08-04 12:12:24 +0900

    GTMコード実装 <head>開始直後 <body>開始直後

--- template-parts/header-default.php
+++ template-parts/header-default.php
@@ -8,6 +8,13 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-K94Z838');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
@@ -15,6 +22,10 @@
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K94Z838"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
 <header id="site-header" class="fixed left-0 top-0 z-50 w-full bg-white shadow-md text-gray-500 group">
   <div class="mx-auto w-full max-w-[1440px] px-6 lg:px-10">
